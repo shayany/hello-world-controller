93a2db53e1417d490bacc4dde8c2f814c061025d
Shayan <shayan.yazdanmehr@gmail.com>
2025-04-05 21:08:42 +0200

    init

---
+++ src/main/java/org/example/webappforazure/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package org.example.webappforazure.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class HelloWorldController {
+
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<String> sayHello(@PathVariable String name) {
+        return new ResponseEntity<String>("Hello, " + name + "!", HttpStatus.OK);
+    }
+}
---
+++ src/test/java/org/example/webappforazure/WebappForAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webappforazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappForAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
